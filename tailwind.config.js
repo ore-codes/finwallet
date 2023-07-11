620b2158e91f792cfd5b9c34494b96a02492ff9a
Ore A <oreadeleye67@gmail.com>
2023-07-11 01:25:59 +0100

    Landing page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,26 @@ export default {
         './resources/js/**/*.jsx',
     ],
 
+    darkMode: 'class',
+
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                brand: {
+                    100: "#c09be9",
+                    200: "#ac79e2",
+                    300: "#9758da",
+                    400: "#8236d3",
+                    500: "#6e28b8",
+                    600: "#5a2197",
+                    700: "#40186c",
+                    800: "#321254",
+                    900: "#1e0b32",
+                }
+            }
         },
     },
 
